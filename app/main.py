a1b3000b70ba7a5b677a0417106d3a94520c9aa1
Sofian Adelin <adelinmihai071@gmail.com>
2025-06-25 18:23:13 +0000

    Init 2.

--- app/main.py
+++ app/main.py
@@ -5,7 +5,11 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    response = b"HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!"
+
+    connection, address = server_socket.accept() # wait for client
+    print(f"got connection from {address}")
+    connection.sendall(response)  # send response
 
 
 if __name__ == "__main__":
