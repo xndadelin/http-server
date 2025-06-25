bc620a935554250774df698e9f02a4f5cdecfc54
Sofian Adelin <adelinmihai071@gmail.com>
2025-06-25 18:41:37 +0000

    Respond with a body.

--- app/main.py
+++ app/main.py
@@ -2,8 +2,6 @@ import socket  # noqa: F401
 
 
 def main():
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     response_ok = b"HTTP/1.1 200 OK\r\n\r\n"
     response_not_found = b"HTTP/1.1 404 Not Found\r\n\r\n"
@@ -24,10 +22,14 @@ def main():
     request_line = request.split(b"\r\n")[0]
     method, path, _ = request_line.split(b" ")
 
-    if method == b"GET" and path == b"/":
-        connection.sendall(response_ok)
-    else:
-        connection.sendall(response_not_found)
+    # respons with body /echo/{str} which must return a 200 OK response
+    # Content-Type and Content-Length are required 
+
+    if method == b"GET" and path.startswith(b"/echo/"):
+        string = path.split(b'/')[2]
+        response = "HTTP/1.1 200 OK\r\n" +  b"Content-Type: text/plain\r\n" + b"Content-Length: " + str(len(string)).encode() + b"\r\n\r\n" + string
+
+        print(response)
 
 
 if __name__ == "__main__":
